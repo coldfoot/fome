59a16e040ad6caddd4a8331cd5db34c7c931d1c5
tiagombp <tiagombp@gmail.com>
2021-11-12 09:40:55 -0300

    ajuste nos triggers

--- vis.js
+++ vis.js
@@ -170,14 +170,18 @@ const v = {
 
                     scrollTrigger: {
                         trigger: '.step3',
-                        markers: false,
+                        markers: true,
                         pin: false,   // pin the trigger element while active
-                        start: "50% bottom", // when the top of the trigger hits the top of the viewport
-                        end: "100% bottom", // end after scrolling 500px beyond the start
+                        start: "top 75%", // when the top of the trigger hits the top of the viewport
+                        end: "bottom 75%", // end after scrolling 500px beyond the start
                         scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-                    }
+                    },
+
+                    //onUpdate : () => { console.log('opa'); }
                 }
-            )
+            );
+
+
 
         }
 
